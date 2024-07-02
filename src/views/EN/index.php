7ace3e60beab298ab30c804b67c1b6b9e051d724
juanbcliment <juanbclimentru@gmail.com>
2024-07-02 11:12:51 -0300

    Datos del CV

--- src/views/EN/index.php
+++ src/views/EN/index.php
@@ -7,76 +7,79 @@
     <!-- header -->
     <?php include("./partialsViews/headerPartial.php"); ?>
     <main>
-        <div class="presentation"></div>
-        <p>I am passionate about learning new technologies and expanding my web development skills.
-            I am currently studying computer engineering and I am specializing in back-end with PHP, Node.js, SQL, phpMyAdmin and Laravel, but I also have knowledge of HTML, CSS, Boostrap, MVC and repository management with Github.
-            I am looking for my first job as a programmer, in a company where I can contribute and continue developing professionally, both in hybrid and remote modes.
-        </p>
+        <div class="presentation">
+            <h2>PHP JavaScript SQL</h2>
+            <p>I am passionate about learning new technologies and expanding my web development skills.</p>
+            <p> I am currently studying computer engineering and I am specializing in back-end with PHP, Node.js, SQL, phpMyAdmin and Laravel,</p>
+            <p>but I also have knowledge of HTML, CSS, Boostrap, MVC and repository management with Github.</p>
+            <p>I am looking for a company where I can contribute and continue developing professionally, both in hybrid and remote modes.</p>
         </div>
         <div class="experience">
-            <h2>Experience</h2>
-
-            <p>
-                Copropietario
-                Kizuna Makers Carpintería artesanal
-                Abril 2019 -Noviembre 2022
-                Planificar el proyectos
-                Coordinar el proyecto
-                Compra de insumos
-                Creación de croquis
-                Creación de planos
-            </p>
-            <p>
-                Técnico en Control de calidad
-                Novatech S.A
-                Agosto 2016 - Octubre 2016
-                Controlar instalación del sistema
-                operativo
-                Comprobación de circuitos electrónicos
-                Optimizar posibles fallas en fabricación
-            </p>
-            <p>
-                Pasantía
-                Gulf Oil Argentina
-                Junio 2014 - Noviembre 2014
-                Mantenimiento preventivo
-                Mantenimiento correctivo
-                Optimizar procesos productivos
-            </p>
-
-            </p>
-            <h2>Education</h2>
-            <p>
-            <p>
-                Formación académica
-                Ingeniería informática
-                Universidad de Palermo
-                Agosto 2023 - Actualidad
-                Cursando actualmente el primer
-                año de la carrera
-            </p>
-            <p>PHP Developer
-                Argentina Programa4.0
-                Mayo 2023 - Noviembre 2023 Graduado
-                Se aprendió el manejo de PHP, Html y CSS para posteriormente aprender Laravel, SQl y
-                Boostrap, para la creación de una pagina web. Creación de una base de datos con
-                phpMyAdmin</p>
-
-            <p>Desarrollo Web Full Stack
-                Digital House
-                Junio 2022 - Diciembre 2022 Graduado
-                Creación de una página web de cero, usando la metodología MVC con Node.js, la base de datos con phpMyAdmin, se aprendió el manejo HTML, CSS y SQL, junto con el manejo de Github para el control de versiones
-            </p>
-            </p>
-            Secundaria
-            Finalizada en Diciembre de 2014
-            Escuela Técnica Alemana Moreno Graduado con título de Técnico Electromecánico
-            <p>
-
-
-
-
+            <div class="job">
+                <h2>Experience</h2>
+                <div class="experience_ind">
+                    <h3>Copropietario </h3>
+                    <h4>Kizuna Makers Carpintería artesanal</h4>
+                    <p>Abril 2019 -Noviembre 2022</p>
+                    <p>Planificar el proyectos</p>
+                    <p>Coordinar el proyecto</p>
+                    <p>Compra de insumos</p>
+                    <p>Creación de croquis</p>
+                    <p>Creación de planos</p>
+                </div>
+                <div class="experience_ind">
+                    <h3>Técnico en Control de calidad</h3>
+                    <h4>Novatech S.A</h4>
+                    <p>Agosto 2016 - Octubre 2016</p>
+                    <p>Controlar instalación del sistema operativo</p>
+                    <p>Comprobación de circuitos electrónicos</p>
+                    <p>Optimizar posibles fallas en fabricación</p>
+                </div>
+                <div class="experience_ind">
+                    <h3>Pasantía</h3>
+                    <h4>Gulf Oil Argentina</h4>
+                    <p>Junio 2014 - Noviembre 2014</p>
+                    <p>Mantenimiento preventivo</p>
+                    <p>Mantenimiento correctivo</p>
+                    <p> Optimizar procesos productivos</p>
+                </div>
+            </div>
+            <div class="academic">
+                <div class="experience_ind">
+                    <h2>Formación académica</h2>
+                    <h3> Ingeniería informática</h3>
+                    <h4>Universidad de Palermo</h4>
+                    <p> Agosto 2023 - Actualidad</p>
+                    <p>Cursando actualmente el primer año de la carrera</p>
+                    <div class="experience_ind">
+                        <h3>PHP Developer</h3>
+                        <h4> Argentina Programa 4.0</h4>
+                        <p> Mayo 2023 - Noviembre 2023 Graduado</p>
+                        <p> Manejo de PHP, Html, CSS, Laravel, SQl y Boostrap</p>
+                        <p>creación de una pagina web</p>
+                        <p>Creación de una base de datos con phpMyAdmin</p>
+                    </div>
+                    <div class="experience_ind">
+                        <h3>Desarrollo Web Full Stack</h3>
+                        <h4>Digital House</h4>
+                        <p>Junio 2022 - Diciembre 2022 Graduado</p>
+                        <p>Creación de una página web de cero con Node.js</p>
+                        <p>metodología MVC</p>
+                        <p>phpMyAdmin, HTML, CSS y SQL</p>
+                        <p>manejo de Github y control de versiones</p>
+                    </div>
+                    <div class="experience_ind">
+                        <h3> Secundaria</h3>
+                        <h4>Escuela Técnica Alemana Moreno</h4>
+                        </p>Finalizada en Diciembre de 2014<p>
+                        <p>Graduado con título de Técnico Electromecánico</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+        <br><br><br><br>
     </main>
 
     <!-- footer -->
