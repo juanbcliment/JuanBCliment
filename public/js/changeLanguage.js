0295eb54f5e29e68363977147eca73f72161fce8
juanbcliment <juanbclimentru@gmail.com>
2024-08-25 21:14:12 -0300

    dark mode save in localstorage

--- public/js/changeLanguage.js
+++ public/js/changeLanguage.js
@@ -1,29 +1,22 @@
 
 const language = document.querySelector(".switch_language");
+
 language.addEventListener("click", func_languag => {
   let activeLanguage_bool = language.classList.toggle("active");
-
   const textsToChange = document.querySelectorAll("[data-section]");
-
    const changeLanguage = async lang => {
-    
       const requestJson = await fetch(`../../public/languages/${lang}.json`);
       const texts = await requestJson.json();
-
       for (const textToChange of textsToChange){
         const section = textToChange.dataset.section;
         const value = textToChange.dataset.value;
         textToChange.innerHTML = texts[section][value]
-      }
-     
-  }
-  
+      }   
+  } 
   if (activeLanguage_bool) {
     changeLanguage("en")
       }else{
         changeLanguage('es')
-      }
-      
-      
-  
+      }     
+ 
 })
--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -2,8 +2,27 @@
 
 const dark_mode = document.querySelector(".switch_dark");
 
-dark_mode.addEventListener("click", func_dark => {
+
+
+document.addEventListener("DOMContentLoaded", e =>{
+    uploadDarkModeLocalStorage()
+    dark_mode.addEventListener("click", toggleDarkMode)
+})
+
+
+function toggleDarkMode(){
     dark_mode.classList.toggle("active_dark");
     document.body.classList.toggle("active_dark")
-} )
+    saveDarkModeStorage(dark_mode.classList.contains("active_dark"))
+}
 
+function saveDarkModeStorage(estado){
+    localStorage.setItem("darkMode", estado)
+}
+function uploadDarkModeLocalStorage(){
+    const darkModeSave = localStorage.getItem("darkMode") === "true";
+    if (darkModeSave){
+        dark_mode.classList.add("active_dark");
+        document.body.classList.add("active_dark")
+    }
+}
