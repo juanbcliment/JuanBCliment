20e418eb0c22662a5bd6d4d96d40f342fa18f85a
´juanbcliment´ <juanbclimentru@gmail.com>
2025-01-16 22:20:37 -0300

    mejoras en el cambio de idioma visual

--- public/js/changeLanguage.js
+++ public/js/changeLanguage.js
@@ -1,9 +1,14 @@
 const languageButton = document.querySelector(".switch_language");
 const textsToChange = document.querySelectorAll("[data-section]");
 
-
 const changeLanguage = async (lang) => {
   try {
+    
+    const currentScrollPosition = window.scrollY;
+
+   
+    document.body.style.visibility = "hidden";
+
     const requestJson = await fetch(`/public/languages/${lang}.json`);
     const texts = await requestJson.json();
 
@@ -13,13 +18,21 @@ const changeLanguage = async (lang) => {
       textToChange.innerHTML = texts[section][value];
     });
 
+   
+    window.scrollTo(0, currentScrollPosition);
 
+  
+    document.body.style.visibility = "visible";
+
+    
     localStorage.setItem("selectedLanguage", lang);
   } catch (error) {
     console.error("Error al cargar el archivo de idioma:", error);
+
+    
+    document.body.style.visibility = "visible";
   }
 };
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const savedLanguage = localStorage.getItem("selectedLanguage");
@@ -31,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (savedLanguage === "en") {
       languageButton.classList.add("active");
     }
-  } else {
+  } else { 
     changeLanguage(defaultLanguage);
   }
 });
@@ -41,7 +54,8 @@ languageButton.addEventListener("click", () => {
 
   if (isActive) {
     changeLanguage("en");
-  } else {
+      } else {
     changeLanguage("es");
+    document.getElementById("english").style.visibility = "hidden";
   }
 });
--- public/js/copy.js
+++ public/js/copy.js
@@ -1,13 +1,15 @@
-document.getElementById('copy-button').addEventListener('click', () => {
 
-    const emailText = document.getElementById('email-text').textContent;
+document.querySelectorAll('.copy-button').forEach(button => {
+    button.addEventListener('click', () => {
+       
+        const targetId = button.getAttribute('data-copy-target');
+        const targetText = document.getElementById(targetId).textContent;
 
-
-    navigator.clipboard.writeText(emailText)
-        .then(() => {
-            alert('¡Email copiado al portapapeles!');
-        })
-        .catch(err => {
-            console.error('Error al copiar:', err);
+      
+        navigator.clipboard.writeText(targetText).then(() => {
+            console.log(`Texto copiado: ${targetText}`);
+        }).catch(err => {
+            console.error('No se pudo copiar el texto: ', err);
         });
+    });
 });
