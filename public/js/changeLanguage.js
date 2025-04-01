5f3ffd95c32bd377360f008e52e27e0f3e229086
´juanbcliment´ <juanbclimentru@gmail.com>
2025-04-01 20:56:59 -0300

    cv en dos idiomas, idoma por navegador, y comiemzo de responsiv

--- public/js/changeLanguage.js
+++ public/js/changeLanguage.js
@@ -1,12 +1,11 @@
 const languageButton = document.querySelector(".switch_language");
 const textsToChange = document.querySelectorAll("[data-section]");
+const cvLink = document.querySelector("#cv-link");
 
 const changeLanguage = async (lang) => {
   try {
     
     const currentScrollPosition = window.scrollY;
-
-   
     document.body.style.visibility = "hidden";
 
     const requestJson = await fetch(`/public/languages/${lang}.json`);
@@ -18,33 +17,38 @@ const changeLanguage = async (lang) => {
       textToChange.innerHTML = texts[section][value];
     });
 
-   
+    if (texts.cv && texts.cv["cv-href"]) {
+      cvLink.href = texts.cv["cv-href"];
+  }
+
     window.scrollTo(0, currentScrollPosition);
-
-  
     document.body.style.visibility = "visible";
-
-    
     localStorage.setItem("selectedLanguage", lang);
   } catch (error) {
     console.error("Error al cargar el archivo de idioma:", error);
-
-    
     document.body.style.visibility = "visible";
   }
 };
 
+const getBrowserLanguage = () => {
+  const navLang = navigator.language || navigator.userLanguage; 
+  const lang = navLang.split('-')[0].toLowerCase(); 
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const savedLanguage = localStorage.getItem("selectedLanguage");
-  const defaultLanguage = "es";
+  const defaultLanguage = "en";
 
   if (savedLanguage) {
     changeLanguage(savedLanguage);
-
     if (savedLanguage === "en") {
       languageButton.classList.add("active");
     }
   } else { 
+    const browserLanguage = getBrowserLanguage();
+    if (browserLanguage === "en" || browserLanguage === "es") {
+        defaultLanguage = browserLanguage; 
+    }
     changeLanguage(defaultLanguage);
   }
 });
