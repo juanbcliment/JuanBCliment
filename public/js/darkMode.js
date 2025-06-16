89f16dc6b548d8628ffebb4fc56bf43a8d5643f1
´juanbcliment´ <juanbclimentru@gmail.com>
2025-06-16 15:26:55 -0300

    mejorando el lighthouse al 100 en todos los campos

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,5 +1,4 @@
 const dark_mode = document.querySelector(".switch_dark");
-const themeIcon = document.getElementById("theme-icon");
 
 document.addEventListener("DOMContentLoaded", (e) => {
   try {
@@ -12,29 +11,19 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 function toggleDarkMode() {
   try {
-
     const currentScrollPosition = window.scrollY;
-    console.log("Scroll guardado (modo oscuro):", currentScrollPosition);
     document.body.style.visibility = "hidden";
 
+  
     dark_mode.classList.toggle("active_dark");
     document.body.classList.toggle("active_dark");
 
-   
-    if (themeIcon.classList.contains("fa-sun")) {
-      themeIcon.classList.remove("fa-sun");
-      themeIcon.classList.add("fa-moon");
-    } else {
-      themeIcon.classList.remove("fa-moon");
-      themeIcon.classList.add("fa-sun");
-    }
-
-    
+  
     saveDarkModeStorage(document.body.classList.contains("active_dark"));
 
-     requestAnimationFrame(() => {
-      requestAnimationFrame(() => { 
-        console.log("Intentando scroll (modo oscuro) a:", currentScrollPosition); 
+   
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
         window.scrollTo(0, currentScrollPosition);
         document.body.style.visibility = "visible";
       });
@@ -42,7 +31,7 @@ function toggleDarkMode() {
 
   } catch (error) {
     console.error("Error al alternar el modo oscuro:", error);
-     document.body.style.visibility = "visible";
+    document.body.style.visibility = "visible";
   }
 }
 
@@ -60,14 +49,8 @@ function uploadDarkModeLocalStorage() {
     if (darkModeSave) {
       dark_mode.classList.add("active_dark");
       document.body.classList.add("active_dark");
-
-      
-      themeIcon.classList.remove("fa-sun");
-      themeIcon.classList.add("fa-moon");
-    } else {
-      themeIcon.classList.remove("fa-moon");
-      themeIcon.classList.add("fa-sun");
     }
+    
   } catch (error) {
     console.error("Error al cargar el estado del modo oscuro desde localStorage:", error);
   }
